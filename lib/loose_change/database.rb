597166e3c18b9aa2e55d6748ada7c2187d069abe
Joshua Miller <elefantstn@gmail.com>
2010-08-18 17:31:49 -0400

    Create a database when it doesn't yet exist.

--- lib/loose_change/database.rb
+++ lib/loose_change/database.rb
@@ -8,10 +8,27 @@ module LooseChange
       @host = server.uri
       @uri  = "/#{name.gsub('/','%2F')}"
       @root = host + uri
+      create_database_unless_exists
     end
     
     def uri
       server.uri + @uri
     end
+    
+    private
+    
+    def create_database_unless_exists
+      unless JSON.parse(RestClient.get("#{server.uri}/_all_dbs", default_headers)).include?(name)
+        RestClient.put uri, "", default_headers
+      end
+    end
+    
+    def default_headers
+      {
+        :content_type => :json,
+        :accept       => :json
+      }
+    end
+
   end
 end
