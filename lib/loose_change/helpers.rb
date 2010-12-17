875f08b79f45142717c7b31398b252c43c7f9817
Joshua Miller <elefantstn@gmail.com>
2010-12-17 17:26:50 -0500

    Added MultiPoint support, some cleanup.  Need to make query generic by spatial function name.

--- lib/loose_change/helpers.rb
+++ lib/loose_change/helpers.rb
@@ -1,5 +1,9 @@
 module LooseChange
   module Helpers
+    def design_document_uri(database, model_name)
+      "#{ database.uri }/_design/#{ CGI.escape(model_name) }"
+    end
+
     def default_headers
       {
         :content_type => :json,
