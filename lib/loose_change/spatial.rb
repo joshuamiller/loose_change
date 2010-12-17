7d5403e4b1ec488acd4cf6e19766e3e0e1ba110e
Joshua Miller <elefantstn@gmail.com>
2010-12-17 17:27:19 -0500

    Fixed last commit.

--- lib/loose_change/spatial.rb
+++ lib/loose_change/spatial.rb
@@ -2,12 +2,34 @@ module LooseChange
   module Spatial
 
     def geo_point(name)
-      design_doc = JSON.parse(RestClient.get("#{ database.uri }/_design/#{ CGI.escape(model_name) }"))
-      RestClient.put("#{ database.uri }/_design/#{ CGI.escape(model_name) }",
-                       design_doc.merge({"spatial" => {"points" => "function(doc) {\n    if (doc.#{name}) {\n        emit({\n            type: \"Point\",\n            coordinates: [doc.#{name}[0], doc.#{name}[1]]\n        }, [doc._id, doc.#{name}]);\n    }};"}}).to_json, default_headers)
+      design_doc = JSON.parse(RestClient.get(design_document_uri(database, model_name)))
+      function =<<JS
+        function(doc) {
+          if (doc.#{name} && doc.model_name == "#{ model_name }") {
+            emit({ type: "Point",
+                   coordinates: doc.#{name} },
+                 [doc._id, doc.#{name}]); }
+        }
+JS
+      RestClient.put(design_document_uri(database, model_name),
+                     design_doc.merge({"spatial" => {"points" => function}}).to_json, default_headers)
       property(name)
     end
-      
+    
+    def geo_multipoint(name)
+      design_doc = JSON.parse(RestClient.get(design_document_uri(database, model_name)))
+      function =<<JS
+        function(doc) {
+          if (doc.#{name} && doc.model_name == "#{ model_name }") {
+            emit({ type: "MultiPoint",
+                   coordinates: doc.#{name} },
+                 [doc._id, doc.#{name}]); }};
+JS
+      RestClient.put(design_document_uri(database, model_name),
+                     design_doc.merge({"spatial" => {"points" => function}}).to_json, default_headers)
+      property(name)
+    end
+    
     def by_bounding_box(lat1, lng1, lat2, lng2)
       JSON.parse(RestClient.get("#{ database.uri }/_design/#{ CGI.escape(model_name) }/_spatial/points?bbox=#{ [lat1,lng1,lat2,lng2].join(',') }", default_headers))['rows'].map do |row|
         find(row['id'])
