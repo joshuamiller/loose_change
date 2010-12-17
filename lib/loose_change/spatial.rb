eaa68931fc35c42e8a0b4ec2204aa3f75c8b4055
Joshua Miller <elefantstn@gmail.com>
2010-12-17 18:10:38 -0500

    Cleaning up spatial function generators.

--- lib/loose_change/spatial.rb
+++ lib/loose_change/spatial.rb
@@ -1,33 +1,23 @@
 module LooseChange
   module Spatial
 
-    def geo_point(name)
-      design_doc = JSON.parse(RestClient.get(design_document_uri(database, model_name)))
-      function =<<JS
+    GEOMETRIES = %w(Point MultiPoint)
+
+    GEOMETRIES.each do |type|
+      define_method("geo_#{type}".downcase.to_sym) do |name|
+        design_doc = JSON.parse(RestClient.get(design_document_uri(database, model_name)))
+        function =<<JS
         function(doc) {
           if (doc.#{name} && doc.model_name == "#{ model_name }") {
-            emit({ type: "Point",
+            emit({ type: "#{type}",
                    coordinates: doc.#{name} },
                  [doc._id, doc.#{name}]); }
         }
 JS
-      RestClient.put(design_document_uri(database, model_name),
-                     design_doc.merge({"spatial" => {name => function}}).to_json, default_headers)
-      property(name)
-    end
-    
-    def geo_multipoint(name)
-      design_doc = JSON.parse(RestClient.get(design_document_uri(database, model_name)))
-      function =<<JS
-        function(doc) {
-          if (doc.#{name} && doc.model_name == "#{ model_name }") {
-            emit({ type: "MultiPoint",
-                   coordinates: doc.#{name} },
-                 [doc._id, doc.#{name}]); }};
-JS
-      RestClient.put(design_document_uri(database, model_name),
-                     design_doc.merge({"spatial" => {name => function}}).to_json, default_headers)
-      property(name)
+        RestClient.put(design_document_uri(database, model_name),
+                       design_doc.merge({"spatial" => (design_doc["spatial"] || {}).merge({ name => function })}).to_json, default_headers)
+        property(name)
+      end
     end
     
     def by_bounding_box(name, lat1, lng1, lat2, lng2)
